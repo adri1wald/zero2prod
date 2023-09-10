446fb56f05b0fee54db02e27028c930f6ead1ef2
Adrien Wald <adrien@genei.io>
2023-09-10 20:58:33 +0100

    feat: add config management

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    let config = get_configuration().expect("Failed to read configuration.");
+    let port = config.application_port;
+    let addr = format!("127.0.0.1:{port}");
+    let listener = TcpListener::bind(addr)?;
     run(listener)?.await
 }
