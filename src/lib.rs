12e2b8c056a0e18fc57cda68891f7a4e178e39db
Adrien Wald <adrien@genei.io>
2023-09-10 19:45:09 +0100

    feat: deserialize form data into struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,29 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
 use std::net::TcpListener;
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
+#[derive(Deserialize)]
+struct SubscribeFormData {
+    name: String,
+    email: String,
+}
+
+async fn subscribe(form: web::Form<SubscribeFormData>) -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
