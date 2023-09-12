cd102991908f5a294a2e586bf07a3313a1ab4f5b
Adrien Wald <adrien@genei.io>
2023-09-13 00:32:00 +0100

    chore: instrument integration tests

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let subscriber = get_subscriber("zero2prod".to_owned(), "info".to_owned());
+    let subscriber = get_subscriber("zero2prod".to_owned(), "info".to_owned(), std::io::stdout);
     init_subscriber(subscriber);
 
     let config = get_configuration().expect("Failed to read configuration.");
